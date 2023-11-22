0b5c8e9e63f3db6c151cdd6a2e0396f31cd08178
HongMinYeong <minyung1240@daum.net>
2023-11-22 15:29:25 +0900

    study: 메소드, 메소드  오버로딩

--- src/_02_control_statement/Practice.java
+++ src/_02_control_statement/Practice.java
@@ -1,7 +1,54 @@
 package _02_control_statement;
 
+import java.util.Scanner;
+
 public class Practice {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자 두 개를 입력하세요 ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("덧셈 결과 : " + sum(num1,num2));
+        System.out.println("뺄셈 결과 : " + subtract(num1,num2));
+        System.out.println("나눈셈 결과: " + div(num1,num2));
+        System.out.println("곱셈 결과: " + mul(num1,num2));
 
+        Practice ol = new Practice();
+        int r = sc.nextInt();
+        System.out.println("반지름이" + r + "인 원의 넓이 : " + ol.surface(r));
+        int a = sc.nextInt(); //가로
+        int b = sc.nextInt(); // 세로
+        double c = sc.nextInt(); //밑변
+        double d = sc.nextInt(); //높이
+        System.out.println("가로 " + a + ",세로 " + b +"인 직사각형의 넓이 : " + ol.surface(a,b));
+        System.out.println("밑변 " + c + ",높이 " + d +"인 삼각형의 넓이 : " + ol.surface(c,d));
+        sc.close();
+    }
+    public static double sum(int x,int y){
+    return x+y;
+    }
+
+    public static double subtract(int x,int y){
+        return x-y;
+    }
+
+    public static double div(double x,double y){
+        return x/y;
+    }
+
+    public static double mul(int x,int y){
+        return x*y;
+    }
+
+    public static double surface(double x){
+        return x*x*Math.PI;
+    }
+
+    public static double surface(int x,int y){
+        return x*y;
+    }
+
+    public static double surface(double x,double y){
+        return x*y/2;
     }
 }
